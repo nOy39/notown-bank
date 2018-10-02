91ff0bea4bae52760e615beba462f5594197fb10
Andrey Popov <popov.a39rus@gmail.com>
2018-10-02 21:28:19 +0200

    first commit empty

---
+++ src/main/java/org/a2lpo/bank/notownbank/NotownBankApplication.java
@@ -0,0 +1,12 @@
+package org.a2lpo.bank.notownbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotownBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotownBankApplication.class, args);
+	}
+}
